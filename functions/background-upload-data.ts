289a59e908981962a1765e3dc8eda199a01c83a8
Params10 <params704@gmail.com>
2023-02-21 21:41:14 +0530

    added scheduled function file

--- functions/background-upload-data.ts
+++ functions/background-upload-data.ts
@@ -1,20 +1,49 @@
-import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
+// YOUR_BASE_DIRECTORY/netlify/functions/test-scheduled-function.ts
 
-const upload_report: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  console.log("Received event:", event);
-  // Logic in English: Attach as a durable queue called reporting to the Filebase Exchange on RabbitMQ
-  // From there, once a day this cron (as a Netlify background function) runs.
-  // Objective: A daily report of all new CIDs, and upload that report back to Filebase
-  // Cronjob ends
-
-  // If this design pattern works well, we'll implement a lot more flows like this (i.e. for Tokens Curate)
-
+import {
+  Handler,
+  HandlerEvent,
+  HandlerContext,
+  schedule,
+} from "@netlify/functions";
+import client, { Connection, Channel, ConsumeMessage } from "amqplib";
+const myHandler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  console.log("Received event:");
+  await rabbitmqconsume("filebase");
   return {
-        statusCode: 200,
-    };
+    statusCode: 200,
+  };
 };
-
-const handler = schedule("@daily", upload_report)
 
 export { handler };
 
+// };
+const handler = schedule("* * * * *", myHandler);
+
+const rabbitmqconsume = async (exchange: any) => {
+  const conn1 = await client.connect(process.env.RABBITMQ_URL);
+  const channel1 = await conn1.createChannel();
+  await channel1.assertExchange(exchange, "fanout", { durable: true });
+  let a = await channel1.assertQueue("queuefilebase", { durable: true });
+  console.log(a);
+  await channel1.bindQueue("queuefilebase", exchange, "", { durable: true });
+  const consumer =
+    (channel: Channel) =>
+    async (msg: ConsumeMessage | null): Promise<void> => {
+      if (msg) {
+        // Display the received message
+        console.log(msg.content.toString());
+        channel.ack(msg);
+      }
+    };
+  console.log("here", a.queue);
+
+  await channel1.consume("queuefilebase", consumer(channel1));
+  console.log("here");
+
+  channel1.close();
+  conn1.close();
+};
