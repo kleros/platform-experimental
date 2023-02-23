b89fd68392f02817eecf0855d7545da69af83154
Params10 <params704@gmail.com>
2023-02-23 13:43:59 +0530

    updated logtail consume function to listen continuously

--- functions/consume-logtail.ts
+++ functions/consume-logtail.ts
@@ -1,47 +1,52 @@
+import amqp, { Message } from "amqplib/callback_api";
 import { Logtail } from "@logtail/node";
-import client, { Connection, Channel, ConsumeMessage, Message } from "amqplib";
 require("dotenv").config();
-const logtail = new Logtail(process.env.LOGTAILKEY || "");
+const logtail = new Logtail( process.env.LOGTAILKEY|| "");
+amqp.connect(
+  process.env.RABBITMQ_URL || "",
+  function (error0: any, connection: any) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1: any, channel: any) {
+      if (error1) {
+        throw error1;
+      }
 
-async function rabbitMqConnection() {
-  // consumer for the queue.
-  // We use currying to give it the channel required to acknowledge the message
-  const promise = new Promise(async (resolve, reject) => {
-    const consumer =
-      (channel: Channel) =>
-      async (msg: ConsumeMessage | null): Promise<void> => {
-        if (msg) {
-          let a = msg.content
-            .toString()
-            .replace(/'/g, '"')
-            .replace("False", "false");
-          console.log(a);
-          await channel.ack(msg);
-          await logtail.info(a);
+      channel.assertQueue(
+        "datalake",
+        {
+          durable: true,
+        },
+        function (error2: any, q: any) {
+          if (error2) {
+            throw error2;
+          }
+          console.log(
+            "[*] Waiting for messages in %s. To exit press CTRL+C",
+          );
+
+          channel.consume(
+            q.queue,
+            async function (msg: any) {
+              if (msg.content) {
+                console.log(" [x] %s", msg.content.toString());
+
+                await logtail.info(
+                   JSON.parse( msg.content
+                      .toString()
+                      .replace(/'/g, '"')
+                      .replace("False", "false")
+                  
+                ));
+              }
+            },
+            {
+              noAck: true,
+            }
+          );
         }
-        resolve(true);
-      };
-    const connection: Connection = await client.connect(
-      process.env.RABBITMQ_URL || ""
-    );
-
-    // Create a channel
-    const channel: Channel = await connection.createChannel();
-
-    // Makes the queue available to the client
-    let k = await channel.assertQueue("datalake");
-
-    // Start the consumer
-    await channel.consume("datalake", consumer(channel));
-  });
-  return promise;
-}
-
-rabbitMqConnection()
-  .then(async () => {
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+      );
+    });
+  }
+);
